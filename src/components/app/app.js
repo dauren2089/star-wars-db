07ffffc59aff7a7e5b5796b608c702a5bab69288
Dauren <gotta_Start@mail.ru>
2020-09-10 22:19:17 +0600

    Routes has been added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,14 +11,20 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import {StarshipDetails} from "../sw-components";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import {StarshipDetails} from '../sw-components';
 
 export default class App extends Component {
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
   };
 
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    });
+  };
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
       const Service = swapiService instanceof SwapiService ?
@@ -30,27 +36,41 @@ export default class App extends Component {
   };
 
   render() {
+    const { isLoggedIn } = this.state;
+
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <Router>
-            <div className="stardb-app">
-              <Header onServiceChange={this.onServiceChange} />
-              <RandomPlanet />
 
-              <Route path="/"
-                     render={() => <h2> Welcome to StarDB </h2> }
-                     exact={true} />
-              <Route path="/people/:id?" component={PeoplePage} />
-              <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" exact component={StarshipsPage} />
-              <Route path="/starships/:id"
-                     render={ ({ match }) => {
-                       const { id } = match.params;
-                       return <StarshipDetails itemId = {id}/>
-                     }}/>
-            </div>
-          </Router>
+            <Router>
+              <div className="stardb-app">
+                <Header onServiceChange={this.onServiceChange} />
+                <RandomPlanet />
+                <Switch>
+                  <Route path="/"
+                         render={() => <h2> Welcome to StarDB </h2> }
+                         exact={true} />
+                  <Route path="/people/:id?" component={PeoplePage} />
+                  <Route path="/planets" component={PlanetsPage} />
+                  <Route path="/starships" exact component={StarshipsPage} />
+                  <Route path="/starships/:id"
+                         render={ ({ match }) => {
+                           const { id } = match.params;
+                           return <StarshipDetails itemId = {id}/>
+                         }}/>
+                  <Route path="/login"
+                         render={ () => ( <LoginPage isLoggedIn={isLoggedIn}
+                                                     onLogin={this.onLogin}/> )}
+                  />
+                  <Route path="/admin"
+                         render={ () => ( <SecretPage  isLoggedIn={isLoggedIn}/> )}
+                  />
+                  {/*Если не один из роутов не сработает, то переходим на главную*/}
+                  {/*<Redirect to="/" />*/}
+                  <Route render={ () => <h2>Page not found</h2>} />
+                </Switch>
+              </div>
+            </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,32 +4,37 @@ import { Link } from "react-router-dom";
 import './header.css';
 
 const Header = ({ onServiceChange }) => {
-  return (
-    <div className="header d-flex">
-      <h3>
-        <Link to="/">
-          StarDB
-        </Link>
-      </h3>
-      <ul className="d-flex">
-        <li>
-            <Link to="/people/">People</Link>
-        </li>
-        <li>
-            <Link to="/planets/">Planets</Link>
-        </li>
-        <li>
-            <Link to="/starships/">Starships</Link>
-        </li>
-      </ul>
-
-      <button
-          onClick={onServiceChange}
-          className="btn btn-primary btn-sm">
-        Change Service
-      </button>
-    </div>
-  );
+    return (
+        <div className="header d-flex">
+            <h3>
+                <Link to="/">
+                    StarDB
+                </Link>
+            </h3>
+            <ul className="d-flex">
+                <li>
+                    <Link to="/people/">People</Link>
+                </li>
+                <li>
+                    <Link to="/planets/">Planets</Link>
+                </li>
+                <li>
+                    <Link to="/starships/">Starships</Link>
+                </li>
+                <li>
+                  <Link to="/login/">Login</Link>
+                </li>
+                <li>
+                  <Link to="/admin/">Admin</Link>
+                </li>
+            </ul>
+            <button
+              onClick={onServiceChange}
+              className="btn btn-primary btn-sm">
+            Change Service
+            </button>
+        </div>
+    );
 };
 
 export default Header;
